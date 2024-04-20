8b9f74caf463e8c4ad69d7926f4be19fdf7a23a2
Prudhvi Raj Grandhe <prudhviraj@Prudhvis-MacBook-Air.local>
2024-04-21 02:23:20 +0530

    new commit

--- src/Compontents/Recordvideo/Recordvideo.js
+++ src/Compontents/Recordvideo/Recordvideo.js
@@ -4,14 +4,9 @@ const Recordvideo = () => {
   useEffect(() => {
     // Event listener for key presses
     document.addEventListener('keydown', event => {
-      // Create FormData object with a placeholder file
-      // const formData = new FormData();
-      // formData.append('image', new File(['placeholder'], 'placeholder.txt'));
 
-      // Send a POST request to save the image when any key is pressed
-      fetch('http://127.0.0.1:5000/save', {
+      fetch('https://cse443backend.onrender.com/save', {
         method: 'POST'
-        // body: formData,
       })
       .then(response => {
         if (!response.ok) {
@@ -29,7 +24,7 @@ const Recordvideo = () => {
   return (
     <div>
       <h1>Live Video Stream</h1>
-      <img src="http://127.0.0.1:5000/video" alt="Live video" />
+      <img src="https://cse443backend.onrender.com/video" alt="Live video" />
     </div>
   );
 };
